ca6a9e960d3dd650b574556820fe7f658f396c51
Kanahiro <mediterranean1769@gmail.com>
2020-08-17 14:51:44 +0900

    fix

--- python/japanmesh/main.py
+++ python/japanmesh/main.py
@@ -257,7 +257,7 @@ if __name__ == "__main__":
             "coordinates": []
         },
         "properties": {
-            "code": 0
+            "code": ""
         }
     }
     for mesh in meshes:
